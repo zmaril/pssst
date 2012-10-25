454e6c392e0b66c74b14b01e3734bb146f5bf0ed
Zack Maril <zack@zacharymaril.com>
2012-10-24 23:23:05 -0200

    Time to attack destrucory

--- src/psssst/core.clj
+++ src/psssst/core.clj
@@ -4,8 +4,11 @@
 
 (def type-record (atom {}))
 
+(defn find-type [param-name param-value]
+  (type param-value))
+
 (defn update-record [fn-name param-names param-values]
-  (let [typed-array (map type param-values)
+  (let [typed-array (doall (map find-type param-names param-values))
         create-or-inc               (fn [v] (if v (inc v) 1))]
     (swap! type-record (fn [m]
                          (update-in m
@@ -13,6 +16,7 @@
                                     create-or-inc)))))
 
 (defn print-type-record []
+  (println type-record-filename)
   (spit type-record-filename
         (with-out-str (clojure.pprint/pprint @type-record))))
 
@@ -25,3 +29,22 @@
      `(defn ~name ~params
         (~record '~name '~params ~params)
         ~@body)))
+
+(defn-recorded plus-one [a b] (+ a b))
+(defn-recorded plus-two [a b c] (+ a b))
+
+(defn run-tests []
+  (plus-one 1 2)
+  (plus-one 1 3)
+  (plus-one 1 4)
+  (plus-one 1 5)
+  (plus-two 1 1 nil)
+  (plus-two 1 2 1)
+  (plus-two 1 3 :a)
+  (plus-two 1 4 'symbol)
+  (plus-two 1 5 {})
+  (plus-two 1 6 [])
+  (plus-two 1 7 #{})
+  (plus-two 1 8 (java.util.Date.)))
+
+(run-tests)
--- type-record-filename.clj
+++ type-record-filename.clj
@@ -1,1 +1,11 @@
-{tester-plus {[a b] {(java.lang.Long clojure.lang.Keyword) 1}}}
+{plus-two
+ {[a b c]
+  {(java.lang.Long java.lang.Long java.util.Date) 2,
+   (java.lang.Long java.lang.Long clojure.lang.PersistentHashSet) 2,
+   (java.lang.Long java.lang.Long clojure.lang.PersistentVector) 2,
+   (java.lang.Long java.lang.Long clojure.lang.PersistentArrayMap) 2,
+   (java.lang.Long java.lang.Long clojure.lang.Symbol) 2,
+   (java.lang.Long java.lang.Long clojure.lang.Keyword) 2,
+   (java.lang.Long java.lang.Long java.lang.Long) 2,
+   (java.lang.Long java.lang.Long nil) 2}},
+ plus-one {[a b] {(java.lang.Long java.lang.Long) 8}}}
