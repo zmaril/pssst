48d0abbdfbe09cb5492b09806f41f0bf8066faa3
Zack Maril <zack@zacharymaril.com>
2012-10-24 23:52:45 -0200

    Moving tests around

--- src/psssst/core.clj
+++ src/psssst/core.clj
@@ -37,36 +37,3 @@
      `(defn ~name ~params
         (~record '~name '~params ~params)
         ~@body)))
-
-(defn-recorded plus-one [a b] (+ a b))
-(defn-recorded plus-two [a b c] (+ a b))
-(defn-recorded plus-three [{:keys [a b c]}] (+ a b))
-(defn-recorded plus-four [{:keys [a b c]} d] (+ a b d))
-(defn-recorded plus-five [[[a] b] c] (+ a b))
-
-(defn run-tests []
-  (plus-one 1 2)
-  (plus-one 1 3)
-
-  (plus-two 1 1 nil)
-  (plus-two 1 2 1)
-  (plus-two 1 3 :a)
-  (plus-two 1 4 'symbol)
-  (plus-two 1 5 {})
-  (plus-two 1 6 [])
-  (plus-two 1 7 #{})
-  (plus-two 1 8 (java.util.Date.))
-
-  (plus-three {:a 1 :b 2})
-  (plus-three {:a 1 :b 2 :c 3})
-  (plus-three {:a 1 :b 2 :c :a})
-
-  (plus-four {:a 1 :b 2 :c 3} 10)
-  (plus-four {:a 1 :b 2 :c :a} 10)
-
-  (plus-five [[1] 2] 3)
-  (plus-five [[1] 10] 10)
-  (plus-five [[1] 2] :key)
-  (plus-five [[1] 2] 'symbol)
-
-  )
--- test/psssst/core_test.clj
+++ test/psssst/core_test.clj
@@ -2,6 +2,37 @@
   (:use clojure.test
         psssst.core))
 
+(defn-recorded plus-one [a b] (+ a b))
+(defn-recorded plus-two [a b c] (+ a b))
+(defn-recorded plus-three [{:keys [a b c]}] (+ a b))
+(defn-recorded plus-four [{:keys [a b c]} d] (+ a b d))
+(defn-recorded plus-five [[[a] b] c] (+ a b))
+
+(defn run-tests []
+  (plus-one 1 2)
+  (plus-one 1 3)
+
+  (plus-two 1 1 nil)
+  (plus-two 1 2 1)
+  (plus-two 1 3 :a)
+  (plus-two 1 4 'symbol)
+  (plus-two 1 5 {})
+  (plus-two 1 6 [])
+  (plus-two 1 7 #{})
+  (plus-two 1 8 (java.util.Date.))
+
+  (plus-three {:a 1 :b 2})
+  (plus-three {:a 1 :b 2 :c 3})
+  (plus-three {:a 1 :b 2 :c :a})
+
+  (plus-four {:a 1 :b 2 :c 3} 10)
+  (plus-four {:a 1 :b 2 :c :a} 10)
+
+  (plus-five [[1] 2] 3)
+  (plus-five [[1] 10] 10)
+  (plus-five [[1] 2] :key)
+  (plus-five [[1] 2] 'symbol))
+
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
